6cd156755ee5f9f84fb3c89a49465a6a70181bbd
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2022-09-20 12:58:45 -0300

    Update script.js

--- Exercício Velocidade média/JS/script.js
+++ Exercício Velocidade média/JS/script.js
@@ -11,13 +11,7 @@ var deslocamento = document.getElementById("deslocamento").value;
 var tempo = document.getElementById("tempo").value;
 z = deslocamento / tempo 
 document.getElementById('resultado').value = z
-}
-
-// function calcular() {
-//     let deslocamento = Number.parseFloat(document.getElementById("deslocamento").value);
-//     let tempo = Number.parseFloat(document.getElementById("tempo").value);
-//     let vm = deslocamento / tempo
-//     document.getElementById("vm").value = vm
-// }
-
 console.log(resultado);
+respostas.push(z)
+document.getElementById('result').innerHTML += `Velocidade Média: ${z} \n` 
+}
